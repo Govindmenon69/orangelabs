79e97aea27b70a7b9f524d6cfdb339e0ca31cf8d
Govind Menon <91384825+Govindmenon69@users.noreply.github.com>
2024-11-11 01:24:33 +0530

    hehe

---
+++ data/index.ts
@@ -0,0 +1,119 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "We prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "We're really flexible with timezones",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Tech stack",
+    description: "We have a HUUUUGEEE",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiasts with a passion for design.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "GaneshPedia 0.0.6!",
+    description: "Whats cooking here,",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Let's build something magnificent?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "GaneshPedia",
+    des: "The opensource project designed and developed to 'troll' Ganesh.",
+    img: "/bs.gif",
+    iconLists: ["/git.svg"],
+    link: "https://ganeshpedia.ddns.net",
+  },
+
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Honestly, Ganeshpedia wouldn't be half as awesome without what I learned at Orange Labs. Those guys really pushed me to think beyond the usual limits. Now, bringing that tech know-how into Ganeshpedia feels like magic—like merging the world of cutting-edge design with, well, the universe of ladoos! We’re making something that’s more than just a page; it's practically a whole new dimension for Ganesh and his fans.",
+    name: "Kavin M.",
+    title: "Assistant Developer @ Syrian Shake LLC",
+  },
+  {
+    quote:
+      "I hate you.",
+    name: "Ganesh Krishna Ramachandran",
+    title: "Supreme Leader @ The country of Syria 🇸🇾 ",
+  },
+
+];
+
+export const companies = [
+  
+];
+
+
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
